fdaa29e8f02fdcd6ad415c0039912fa2d3438d25
Xihai Luo <xluo@polaronsolar.com>
2025-05-15 10:07:11 -0400

    fix(financeit): new logo and mobile responsiveness

--- src/components/layout/TopBanner.js
+++ src/components/layout/TopBanner.js
@@ -1,15 +1,21 @@
-import React from 'react';
-//import FinanceItLogo from '../../images/logo/FinanceIt-Logo.png';
+import React from "react";
+//import FinanceItLogo from "../../images/logo/financeit-logo.svg";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function TopBanner() {
   return (
-    <div className="flex justify-between items-center px-16 py-2 bg-slate-200 max-md:px-8 max-md:py-2 max-sm:px-4 max-sm:py-2">
-      <h1 className="text-sm font-bold text-gray-950 max-sm:mx-auto">
-        Finance It Logo
-      </h1>
+    <div className="flex max-mobile2:flex-col max-mobile2:gap-4 justify-between items-center px-16 py-2 bg-slate-200 max-md:px-8 max-md:py-2 max-sm:px-4 max-sm:py-2">
+      <StaticImage
+        src={"../../images/logo/financeit-logo.svg"}
+        alt="Finance it Logo"
+        layout="fixed"
+        width={100}
+        height={15}
+      />
       <div className="flex flex-col gap-1 items-center">
         <p className="text-xs text-center text-gray-950">
-          Take it easy. Don't pay for 3 months. Get an easy, dependable payment plan.
+          Take it easy. Don't pay for 3 months. Get an easy, dependable payment
+          plan.
         </p>
         <p className="text-sm font-bold text-center text-gray-950">
           ( Lowest Monthly Payment | Don't pay for 3 months | Easy Apply )
